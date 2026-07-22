b63aabcdae6a7d1310274e641100c40b727bfb2a
PencilFluke <willhobden93@gmail.com>
2026-07-22 10:21:21 -0400

    Total cost component

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -43,7 +43,7 @@ const ConferenceEvent = () => {
     const handleMealSelection = (index) => {
         const item = mealsItems[index];
 
-        if(item.selected && item.type === "mealsForPeople"){
+        if(item.selected && item.type === "mealForPeople"){
             const newNumberOfPeople = item.selected ? numberOfPeople : 0;
             dispatch(toggleMealSelection(index,newNumberOfPeople));
         } else {
@@ -53,12 +53,65 @@ const ConferenceEvent = () => {
 
     const getItemsFromTotalCost = () => {
         const items = [];
+        venueItems.forEach((item) => {
+            if(item.quantity > 0) {
+                items.push({...item,type:"venue"});
+            }
+        });
+        avItems.forEach((item) => {
+            if(item.quantity > 0 &&
+            !items.some((i) => i.name === item.name && i.type === "av")) {
+                items.push({...item,type:"av"});
+            }
+        });
+        mealsItems.forEach((item) => {
+            if(item.selected) {
+                const itemForDisplay = { ...item, type: "meals" };
+                if(item.numberOfPeople){
+                    itemForDisplay.numberOfPeople = numberOfPeople;
+                }
+                items.push(itemForDisplay);
+            }
+        });
+        return items;
     };
 
     const items = getItemsFromTotalCost();
 
     const ItemsDisplay = ({ items }) => {
+        console.log(items);
+        return <>
+            <div className="display_box1">{items.length === 0 && <p>No items selected</p>}
+                <table className="table_item_data">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Unit Cost</th>
+                            <th>Quantity</th>
+                            <th>Subtotal</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {items.map((item, index) => (
+                            <tr key={index}>
+                                <td>{item.name}</td>
+                                <td>${item.cost}</td>
+                                <td>
+                                    {item.type === "meals" || item.numberOfPeople
+                                    ? ` For ${numberOfPeople} people`
+                                    : item.quantity}
+                                </td>
+                                <td>{item.type === "meals" || item.numberOfPeople
+                                    ? `${item.cost * numberOfPeople}`
+                                    : `${item.cost * item.quantity}`}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
 
+            </div>
+        </>
     };
     const calculateTotalCost = (section) => {
         let totalCost = 0;
@@ -80,6 +133,12 @@ const ConferenceEvent = () => {
     const venueTotalCost = calculateTotalCost("venue");
     const avTotalCost = calculateTotalCost("av");
     const mealsTotalCost = calculateTotalCost("meals");
+
+    const totalCosts = {
+        venue: venueTotalCost,
+        av: avTotalCost,
+        meals: mealsTotalCost,
+    }
 
     const navigateToProducts = (idType) => {
         if (idType == '#venue' || idType == '#addons' || idType == '#meals') {
@@ -225,7 +284,7 @@ const ConferenceEvent = () => {
                                     {mealsItems.map((item,index)=>(
                                         <div className="meal_item" key={index} style={{padding:15}}>
                                             <div className="inner">
-                                                <input type="text" id={`meal_${index}`}
+                                                <input type="checkbox" id={`meal_${index}`}
                                                 checked={item.selected}
                                                 onChange={()=>handleMealSelection(index)}/>
                                                 <label htmlFor={`meal_${index}`}>{item.name}</label>
